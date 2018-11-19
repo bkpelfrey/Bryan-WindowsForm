81e4d98d70686931a7a8afdc254b347e3ac6558d
Bryan Pelfrey <bpelfrey@innovasi.com>
2018-11-19 11:14:35 -0500

    Initial application

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Button button1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1 = new Button();
+            button1.Size = new Size(40, 40);
+            button1.Location = new Point(30, 30);
+            button1.Text = "Click Me";
+            this.Controls.Add(button1);
+            button1.Click += new EventHandler(button1_Click);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world!");
+        }
+
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new Form1());
+
+        }
+
+    }
+}
